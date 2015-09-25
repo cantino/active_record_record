4052935dcf8800f60b1cc7835ea9a137f0733d52
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-25 13:21:41 -0700

    Drop generated spec

--- spec/active_record_record_spec.rb
+++ spec/active_record_record_spec.rb
@@ -4,8 +4,4 @@ describe ActiveRecordRecord do
   it 'has a version number' do
     expect(ActiveRecordRecord::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
