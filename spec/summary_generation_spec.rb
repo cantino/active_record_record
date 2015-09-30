60afcad09ced22db7954f034f415b87336f04848
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-29 19:07:56 -0700

    Add spec for clearing the ar counts

--- spec/summary_generation_spec.rb
+++ spec/summary_generation_spec.rb
@@ -1,25 +1,30 @@
-require 'spec_helper'
+require "spec_helper"
+require_relative "../lib/active_record_record/summary_generation"
 
-define ActiveRecordRecord::SummaryGeneration do
-  define "#clear_ar_counts" do
+describe ActiveRecordRecord::SummaryGeneration do
+ describe "#clear_ar_counts" do
     before do
+      Thread.current[:request_start_time] = "00:00:00"
       Thread.current[:ar_counts] = { test: "I love ar_counts" }
       Thread.current[:times] = { times: "Are changing" }
-      Thread.current[:request_start_time] = "This is the start time, trust me"
       Thread.current[:do_counts] = false
       Thread.current[:objects_key] = :silly_user_key
+
+      class StubClass
+        include ActiveRecordRecord::SummaryGeneration
+      end
+      StubClass.new.clear_ar_counts
     end
 
     it "will reset all thread current variables" do
+      expect(Thread.current[:request_start_time]).to_not eq("00:00:00")
       expect(Thread.current[:ar_counts]).to eq({})
       expect(Thread.current[:times]).to eq({})
-      # We should use time cop
-      # expect(Thread.current[:request_start_time]).to eq(Time.now)
       expect(Thread.current[:do_counts]).to eq(true)
       expect(Thread.current[:objects_key]).to eq(:default)
     end
   end
 
-  define "#dump_counts"
-  define "#print_ar_counts"
+  describe "#dump_counts"
+  describe "#print_ar_counts"
 end
