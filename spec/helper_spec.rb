02b4ffebb5ddae624dd5a9693caed9e1a27466cd
Greg McGuirk <bert.mcguirk@gmail.com>
2015-10-09 08:38:49 -0700

    add spec for

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -1,46 +1,60 @@
 require 'spec_helper'
+require_relative "../lib/active_record_record/helper"
 
 describe ActiveRecordRecord::Helper do
-    # let(:caller) do  
-    #   [
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:355:in `eval'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:355:in `evaluate_ruby'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:323:in `handle_line'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:243:in `block (2 levels) in eval'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:242:in `catch'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:242:in `block in eval'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:241:in `catch'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:241:in `eval'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:77:in `block in repl'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:67:in `loop'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:67:in `repl'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:38:in `block in start'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/input_lock.rb:61:in `call'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/input_lock.rb:61:in `__with_ownership'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/input_lock.rb:79:in `with_ownership'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:38:in `start'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:15:in `start'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_class.rb:169:in `start'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:219:in `block in <top (required)>'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:83:in `call'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:83:in `block in parse_options'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:83:in `each'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:83:in `parse_options'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/bin/pry:16:in `<top (required)>'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/bin/pry:23:in `load'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/bin/pry:23:in `<main>'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/bin/ruby_executable_hooks:15:in `eval'",
-    #     "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/bin/ruby_executable_hooks:15:in `<main>'"
-    #   ]
-    # end 
-  #
-  describe ".track_time_as"
-    context "when in a rails developement environment"
-      context "and COUNT_OBJECTS is true"
-        it "creates thread current hash keyed with :times"
-        it "creates thread current hash tracking sum or count"
-        context "when sum or count have not yet be set"
-            it "they will be defaulted to 0"
+  let(:caller) do
+    [
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:355:in `eval'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:355:in `evaluate_ruby'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:323:in `handle_line'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:243:in `block (2 levels) in eval'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:242:in `catch'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:242:in `block in eval'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:241:in `catch'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_instance.rb:241:in `eval'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:77:in `block in repl'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:67:in `loop'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:67:in `repl'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:38:in `block in start'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/input_lock.rb:61:in `call'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/input_lock.rb:61:in `__with_ownership'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/input_lock.rb:79:in `with_ownership'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:38:in `start'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/repl.rb:15:in `start'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/pry_class.rb:169:in `start'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:219:in `block in <top (required)>'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:83:in `call'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:83:in `block in parse_options'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:83:in `each'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/lib/pry/cli.rb:83:in `parse_options'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/gems/pry-0.10.1/bin/pry:16:in `<top (required)>'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/bin/pry:23:in `load'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/bin/pry:23:in `<main>'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/bin/ruby_executable_hooks:15:in `eval'",
+      "/Users/fredflinstone/.rvm/gems/ruby-2.1.5@mavenlink/bin/ruby_executable_hooks:15:in `<main>'"
+    ]
+  end
+
+  describe ".track_time_as" do
+    context "when in a rails developement environment" do
+      context "and COUNT_OBJECTS is true" do
+        before do
+          Rails = double("Rails", env: double("Faker",  development?: true))
+          expect(Rails.env.development?).to be_truthy
+          ENV['COUNT_OBJECTS'] = "true"
+          Thread.current[:times] = {}
+        end
+
+        it "creates thread current hash keyed with :times and a given input key" do
+          ActiveRecordRecord::Helper.track_time_as(:spaghetti) { :meatball }
+
+          expect(Thread.current[:times][:spaghetti]).to be_truthy
+          expect(Thread.current[:times][:spaghetti][:count]).to eq(1)
+          expect(Thread.current[:times][:spaghetti][:sum]).to be_truthy
+        end
+      end
+    end
+  end
 
     context "when in developement environment"
       context  "and COUNT_OBJECTS is false"
@@ -50,7 +64,7 @@ describe ActiveRecordRecord::Helper do
     context "when not in a rails developement environment"
       context  "and COUNT_OBJECTS is false"
 
-  describe ".use_ar_count_key" 
+  describe ".use_ar_count_key"
     context "given a count key"
       it "will use the new count key for the duration of one block only"
 
