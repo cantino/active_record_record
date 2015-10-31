a780289bb0e3150de582815731db2502ed068ca6
Greg McGuirk <bert.mcguirk@gmail.com>
2015-10-30 18:57:05 -0700

    remove rail tie, this will be spec'd at the integration level in the future

--- spec/railtie_spec.rb
+++
@@ -1,8 +0,0 @@
-describe Railtie do
-  context "ActiveSupport on_load behaviors"
-    it "adds ActiveRecordRecord to ActionController"
-    it "addes TimeTracker::Helper to ActiveSupport"
-
-  context "When user sets 'COUNT_OBJECTS'"
-    it "will require monkey_patcher"
-end
